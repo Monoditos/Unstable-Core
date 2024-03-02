ed003856d0efe24ce28afd308771a852c8702454
GuilhermeSantos11 <104851346+GuilhermeSantos11@users.noreply.github.com>
2024-03-02 11:25:58 +0000

    verificacoes mini jogo codigo
    mini jogo codigo funcional mas muito feio

--- Assets/_Scripts/Tasks/HexCode/StartCode.cs
+++ Assets/_Scripts/Tasks/HexCode/StartCode.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class StartCode : MonoBehaviour
 {
-
+    public TMP_Text wordTxt;
+    public TMP_InputField answerTxt;
+    Codes codeUsed;
     public List<Codes> codeList = new List<Codes>();
+    private void Start()
+    {
+        codeUsed = GetCode();
+        Debug.Log("Code used: " + codeUsed.word);
+        wordTxt.text = codeUsed.word;
+    }
     Codes GetCode()
     {
 
         int randomNum = Random.Range(1, 101);
+        Debug.Log("Random number: " + randomNum);
         List<Codes> possibleCodes = new List<Codes>();
         foreach (Codes code in codeList)
         {
@@ -38,6 +49,16 @@ public class StartCode : MonoBehaviour
         }
     }
 
-
+    public void CheckAnswer()
+    {
+        if (answerTxt.text == codeUsed.answer)
+        {
+            Debug.Log("Correct");
+        }
+        else
+        {
+            Debug.Log("Incorrect");
+        }
+    }
 
 }
