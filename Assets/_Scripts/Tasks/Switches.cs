a71b9c114ee961b5b43cfb70b313f0b8fbfe95af
GuilhermeSantos11 <104851346+GuilhermeSantos11@users.noreply.github.com>
2024-03-02 03:35:26 +0000

    interruptor q aparece no jogo
    o outro n tava a aparecer por razoes misticas (juro q n era por eu n ter usado cenas da UI :P)

--- Assets/_Scripts/Tasks/Switches.cs
+++ Assets/_Scripts/Tasks/Switches.cs
@@ -4,6 +4,84 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Switches : MonoBehaviour
 {
+    public GameObject on, off;
+    public int counter;
+    public bool isOn;
+    public float randomValue, randomChange;
+
+    void Start()
+    {
+        randomValue = Random.value;
+        if (randomValue < 0.40)
+        {
+            isOn = true;
+        }
+        else
+        {
+            isOn = false;
+        }
+        counter = SwitchCount.GetSwitches;
+        on.SetActive(isOn);
+        off.SetActive(!isOn);
+        if (isOn)
+        {
+            SwitchCount.AddSwitch(1);
+        }
+    }
+
+    private void Update()
+    {
+        StartCoroutine(RandomlySelectFalseEachSecond());
+        counter = SwitchCount.GetSwitches;
+    }
+    private void OnMouseUp()
+    {
+        isOn = !isOn;
+        on.SetActive(isOn);
+        off.SetActive(!isOn);
+        if (isOn)
+        {
+            SwitchCount.AddSwitch(1);
+        }
+        else
+        {
+            SwitchCount.AddSwitch(-1);
+        }
+    }
+
+    IEnumerator RandomlySelectFalseEachSecond()
+    {
+        randomChange = Random.value;
+        Debug.Log(randomChange);
+        if (randomChange <= 0.001 && isOn == true)
+        {
+            isOn = false;
+            on.SetActive(isOn);
+            off.SetActive(!isOn);
+            SwitchCount.AddSwitch(-1);
+            yield return new WaitForSeconds(1.5f);
+        }
+        yield return new WaitForSeconds(1.5f);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Switches : MonoBehaviour
+{
     public SpriteRenderer on, off;
     public int counter;
     public bool isOn;
@@ -63,4 +141,4 @@ public class Switches : MonoBehaviour
         }
         yield return new WaitForSeconds(1.5f);
     }
-}
+}*/
