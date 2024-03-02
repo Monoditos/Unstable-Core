2442bf56e5072833e68be8d0322ff3ba2dbc2287
Pedro Sousa <83694082+PRCSousa@users.noreply.github.com>
2024-03-02 02:42:09 +0000

    mapa mais eerie

---
+++ Assets/Scripts/Terminal.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Terminal : MonoBehaviour
+{
+    public GameObject consoleTxt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
